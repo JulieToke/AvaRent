06b421a94604e291a5207d6161b3318105e0aadf
Steve O'Callaghan <steve.ocallaghan@avanade.com>
2022-06-08 21:25:21 +1000

    Upgrades to NewTenant.js

--- Avanade.Core/ReactWebsiteDay1/ClientApp/src/components/NewTenant.js
+++ Avanade.Core/ReactWebsiteDay1/ClientApp/src/components/NewTenant.js
@@ -1,5 +1,5 @@
 ﻿import { Fragment, useState } from "react";
-import { Form, Button, Input, FormGroup, Label } from 'reactstrap';
+import { Form, Button, Input, FormGroup, Label, Col } from 'reactstrap';
 
 const NewTenant = ()  => {
 
@@ -14,24 +14,120 @@ const NewTenant = ()  => {
     }
 
     return <Fragment>
+
         {/*<div><p>I am a Counter and my Count is {counter}</p></div>*/}
-
-
         {/*<div><p><Button color="Danger" onClick={increaseCounter}>Increase Counter by 1</Button></p></div>*/}
         {/*<div><p><button onClick={multiplyCounter}>Multiply Counter by 2</button></p></div>*/}
-
+        
         <Form>
-            <h4>Your Details</h4>
-            <p><Input type="text" placeholder="Name"></Input></p>
-            <p><Input type="text" placeholder="Address"></Input></p>
-            <h4>Requirements</h4>
+            
+            <h4>Your Details</h4>            
+                
             <FormGroup row>
-                <Label>Bedrooms</Label>
-            <p><Input type="text" placeholder="Bedrooms"></Input></p>
-                <Button type="submit" className="success">Submit</Button>
+                <Label 
+                    for="exampleEmail"
+                    sm={2}
+                    >
+                    Email
+                </Label>
+                <Col sm={10}>
+                    <Input
+                        id="exampleEmail"
+                        name="email"
+                        placeholder="Email"
+                        type="email"
+                    />
+                </Col>
             </FormGroup>
 
-        </Form>
+            <FormGroup row>
+                <Label 
+                    for="examplePassword"
+                    sm={2}
+                    >
+                    Password
+                </Label>
+                <Col sm={10}>
+                    <Input
+                        id="examplePassword"
+                        name="password"
+                        placeholder="Password"
+                        type="password"
+                    />
+                </Col>
+            </FormGroup>
+
+            <h4>Preferences</h4>
+
+            <FormGroup row>
+                <Label
+                for="exampleSelect"
+                sm={2}
+                >
+                Bedrooms
+                </Label>
+                <Col sm={10}>
+                <Input
+                    id="exampleSelect"
+                    name="select"
+                    type="select"
+                >
+                    <option>
+                    1
+                    </option>
+                    <option>
+                    2
+                    </option>
+                    <option>
+                    3
+                    </option>
+                    <option>
+                    4
+                    </option>
+                    <option>
+                    5
+                    </option>
+                </Input>
+                </Col>
+            </FormGroup>
+
+            <FormGroup row>
+                <Label
+                for="exampleSelect"
+                sm={2}
+                >
+                Bathrooms
+                </Label>
+                <Col sm={10}>
+                <Input
+                    id="exampleSelect"
+                    name="select"
+                    type="select"
+                >
+                    <option>
+                    1
+                    </option>
+                    <option>
+                    2
+                    </option>
+                    <option>
+                    3
+                    </option>
+                    <option>
+                    4
+                    </option>
+                    <option>
+                    5
+                    </option>
+                </Input>
+                </Col>
+            </FormGroup>
+
+            <Button>
+                Submit
+            </Button>
+
+         </Form>
 
     </Fragment>
 
