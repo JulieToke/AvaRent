2758f1d5ee742821fa360f3433e4ca283601c39e
Steve O'Callaghan <steve.ocallaghan@avanade.com>
2022-06-08 16:28:35 +1000

    Initial Commit

---
+++ Avanade.Core/CodeExamples/Pyramids.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeExamples
+{
+    public class Pyramids
+    {
+        /// <summary>
+        /// For a given height - Write to the console output half a piramid or a half triangle
+        /// *                                                                                                    
+        /// **                                                                                                 
+        /// ***
+        /// ****
+        /// </summary>
+        /// <param name="height"></param>
+        public void WriteHalfPiramid(int height)
+        {
+            Console.WriteLine("*");
+        }
+
+        /// <summary>
+        /// For a given height - Write to the console output a piramid or a triangle
+        ///    *                                                                                                    
+        ///   ***                                                                                                 
+        ///  *****
+        /// ********
+        /// </summary>
+        /// <param name="height"></param>
+        public void WritePiramid(int height)
+        {
+        }
+
+        /// <summary>
+        /// For a given height - Write to the console output a numeric piramid or a triangle
+        ///    1                                                                                                    
+        ///   222                                                                                                 
+        ///  33333
+        /// 4444444
+        /// </summary>
+        /// <param name="height"></param>
+        public void WriteNumericPiramid(int height)
+        {
+        }
+    }
+}
---
+++ Avanade.Core/Launcher/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLaunch = new System.Windows.Forms.Button();
+            this.txbResult = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.Location = new System.Drawing.Point(12, 44);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(142, 46);
+            this.btnLaunch.TabIndex = 0;
+            this.btnLaunch.Text = "Sum";
+            this.btnLaunch.UseVisualStyleBackColor = true;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            // 
+            // txbResult
+            // 
+            this.txbResult.Location = new System.Drawing.Point(181, 44);
+            this.txbResult.Multiline = true;
+            this.txbResult.Name = "txbResult";
+            this.txbResult.Size = new System.Drawing.Size(447, 394);
+            this.txbResult.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 96);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 46);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Substract";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(142, 46);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Multiply";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 200);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(142, 46);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Divide";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txbResult);
+            this.Controls.Add(this.btnLaunch);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLaunch;
+        private System.Windows.Forms.TextBox txbResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
