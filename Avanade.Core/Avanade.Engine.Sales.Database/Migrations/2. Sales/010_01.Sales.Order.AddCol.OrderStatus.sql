a77f5ec9c0a63fff6173d987f154d1f1980d2633
Steve O'Callaghan <steve.ocallaghan@avanade.com>
2022-06-09 00:07:47 +0000

    Deleted Avanade.Core

--- Avanade.Core/Avanade.Engine.Sales.Database/Migrations/2. Sales/010_01.Sales.Order.AddCol.OrderStatus.sql
+++
@@ -1,9 +0,0 @@
-﻿ALTER TABLE [Sales].[Order]
-ADD OrderStatusID INT NOT NULL;
-
-ALTER TABLE [Sales].[Order]  WITH NOCHECK ADD  CONSTRAINT [FK_OrderOrderStatus] FOREIGN KEY(OrderStatusID)
-REFERENCES Ref.OrderStatus (OrderStatusID)
-GO
-
-ALTER TABLE [Sales].[Order] CHECK CONSTRAINT [FK_OrderOrderStatus]
-GO
